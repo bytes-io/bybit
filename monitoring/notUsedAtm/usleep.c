ed345a383cb7f2d1d0a432b688839d07201a6b03
samnet <samnet@users.noreply.github.com>
2018-06-13 14:07:13 +0100

    Delete usleep.c

--- monitoring/notUsedAtm/usleep.c
+++
@@ -1,7 +0,0 @@
-// not used because GNU's sleep version  6.12 (at least and above) accepts floating point arguments. Could be used on a machine that does not have this fcn.
-
-#include <unistd.h>
-#include <stdlib.h>
-int main(int argc, char **argv){
-	usleep(atol(arfv[1]));
-}
